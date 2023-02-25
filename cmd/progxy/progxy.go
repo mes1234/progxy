c57e774a4e2b293679d3c0db67498dea52f35026
WK WK <witold.kepczynski@gmail.com>
2023-02-25 23:11:40 +0100

    added test project

---
+++ cmd/progxy/progxy.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/mes1234/progxy/internal/worker"
+	"github.com/mes1234/progxy/service/configuration"
+)
+
+func main() {
+
+	// Create main context
+	mainCtx := context.Background()
+
+	// Application context able to capture system signals
+	appCtx, appCancel := context.WithCancel(mainCtx)
+
+	// exit procedure
+	exit(appCancel)
+
+	cs := configuration.NewConfigurationService()
+
+	cs.Init(configuration.DefaultPath, configuration.NoActionCallback)
+
+	cs.GetLogger().Info("PROGXY bootstrap")
+
+	run(cs, appCtx)
+
+	<-mainCtx.Done()
+}
+
+func exit(appCancel context.CancelFunc) {
+	c := make(chan os.Signal, 10)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		appCancel()
+	}()
+}
+
+func run(cs configuration.ConfigurationService, ctx context.Context) {
+
+	destinations := cs.GetDestinations()
+
+	logger := cs.GetLogger()
+
+	logger.Info("Started Progxy HAVE FUN")
+
+	adapters := make(map[string]worker.TcpAdapter)
+
+	for key, destination := range destinations {
+		adapters[key] = worker.NewTcpAdaper(
+			destination.Proxied,
+			destination.Port,
+			net.Listen,
+			net.Dial,
+			net.LookupIP,
+			logger,
+			ctx)
+	}
+}
--- test/integration/script.js
+++ test/integration/script.js
@@ -4,6 +4,6 @@ import { sleep } from "k6";
 export default function () {
     // http.get('http://127.0.0.1:9100/test/integration/index.html');
     // http.get('http://127.0.0.1:9200');
-    http.get('http://127.0.0.1:80');
+    http.get('http://127.0.0.1:9700/ping');
     sleep(0.01);
 }
